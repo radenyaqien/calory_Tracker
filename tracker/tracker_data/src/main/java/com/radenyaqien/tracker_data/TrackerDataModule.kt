f5082accd5013faf5c00f6fdc29f3f8bfcad0f13
Muhammad Ainul Yaqin <myaqien27@gmail.com>
2023-04-15 16:46:53 +0700

    Setup OpenFoodApi

---
+++ tracker/tracker_data/src/main/java/com/radenyaqien/tracker_data/TrackerDataModule.kt
@@ -0,0 +1,40 @@
+package com.radenyaqien.tracker_data
+
+import com.radenyaqien.tracker_data.remote.OpenFoodApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TrackerDataModule {
+
+
+    @Provides
+    @Singleton
+    fun provideOkhttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().addInterceptor(
+            HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesApi(okHttpClient: OkHttpClient): OpenFoodApi {
+        return Retrofit.Builder()
+            .baseUrl(OpenFoodApi.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create(OpenFoodApi::class.java)
+    }
+
+}
---
+++ tracker/tracker_data/src/main/java/com/radenyaqien/tracker_data/remote/dto/SearchDto.kt
@@ -0,0 +1,5 @@
+package com.radenyaqien.tracker_data.remote.dto
+
+data class SearchDto(
+    val products: List<Product>
+)
